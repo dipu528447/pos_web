aaa2efb7f567fd8c2f90c66c0b97e93bc9294204
dipu528447 <dipankargupta08@gmail.com>
2026-04-21 19:18:52 +0600

    backend ready

--- modules/auth/routes.js
+++ modules/auth/routes.js
@@ -4,6 +4,8 @@ const authController = require("./controller");
 
 
 router.post("/register", authController.register);
-router.post("/login", authController.login);
-
+// router.post("/login", authController.login);
+router.post("/login", (req, res) => {
+  res.json({ message: "Login working" });
+});
 module.exports = router;
